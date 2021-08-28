a99e0c1326c2cb5168624ca0d68f7ab41e6178e0
sam <samueldazadaza@gmail.com>
2021-08-28 11:59:22 -0500

    add var port 3000

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+//settings
+app.set('port', 3000); //variable del puerto
+
 // milddlewares
 app.use(morgan('dev'));
-app.use(morgan('combined'));
+app.use(express.urlencoded({extended: false})); //para que el server entienda valor de inputs en un formulario
+app.use(express.json()); //para recibir formato json
+
 
 //starting the server
-app.listen(3000, () => {
-    console.log(`server on port ${3000} in live`)
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
 })
