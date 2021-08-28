01aabc750febfb221a610a5615442ba0a6e49172
sam <samueldazadaza@gmail.com>
2021-08-28 11:52:08 -0500

    watch data for petition get

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// milddlewares
+app.use(morgan('dev'));
+app.use(morgan('combined'));
+
+//starting the server
 app.listen(3000, () => {
     console.log(`server on port ${3000} in live`)
 })
